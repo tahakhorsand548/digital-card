dbf6b25859320dc1b0961efaa249c027c77309bd
taha <tahakhorsand548@gmail.com>
2026-07-12 22:57:34 +0330

    editloginClor2

--- src/components/AuthPages.tsx
+++ src/components/AuthPages.tsx
@@ -233,8 +233,7 @@ export default function AuthPages({ onLoginSuccess, initialMode = "login" }: Aut
   };
 
   return (
-    <div className="min-h-screen bg-slate-950 flex flex-col items-center justify-center px-4 py-8 relative w-72 h-72 rounded-full">
-      
+<div className="min-h-screen bg-slate-950 rounded-[32px] overflow-hidden flex flex-col items-center justify-center px-4 py-8 relative">      
       {/* Background visual glows */}
       <div className="absolute top-1/4 left-1/3 w-72 h-72  rounded-full bg-blue-600/10 blur-[100px] pointer-events-none glow-animation bg-slate-950" />
       <div className="absolute bottom-1/4 right-1/3 w-72 h-72 rounded-full bg-indigo-600/10 blur-[100px] pointer-events-none glow-animation" />
